f619b9649fe3a86523e174636b397143f1cbe380
Priyajha7585 <priyajha7585@gmail.com>
2023-02-03 13:57:33 +0530

    Worked on form validation design

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -4,18 +4,17 @@ import CreateNew from '../../assets/Group 11.png'
 import Image from 'next/image';
 import Popup from 'reactjs-popup';
 import {CgClose} from 'react-icons/cg';
-import {useForm} from 'react-hook-form'
-
+import {useForm} from 'react-hook-form';
+import { Tooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css'
 
 const PopupWindow = ({path,router}) => {
     const {register, handleSubmit, formState:{errors}, reset} = useForm();
 
-    const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]:e.target.value.trim()
-        });
-    };
+    const onSubmit = (data) => {
+        data['subFilesAndFolders'] = []
+        createFolder(data)
+    }
 
     const createFolder = async(formData) => {
         const response = await fetch(path===undefined ? '/api' : '/api/root/'+path.join("/"), {
@@ -26,18 +25,12 @@ const PopupWindow = ({path,router}) => {
             },
         })
         const data = await response.json();
-        // console.log("Posted data")
-        // console.log(data)
         reset()
         if(response.status < 300)
         {
             router.replace(router.asPath)
         }
 
-    }
-    const onSubmit = (data) => {
-        data['subFilesAndFolders'] = []
-        createFolder(data)
     }
   return (
         <Popup trigger=
@@ -49,22 +42,22 @@ const PopupWindow = ({path,router}) => {
                         Create New <button onClick={()=>close()}><CgClose className={styles.popup_close} /></button>
                     </div>
                     <form onSubmit={handleSubmit(onSubmit)}>
-                        <div className={styles.popup_checkbox_button}>
+                        <div id="my-anchor-element" className={styles.popup_checkbox_button}>
                             <input type="radio" id="file" name="type" value="file" {...register('type', {required:"Type is required!!!"})}/>
                             <label htmlFor="file">File</label>
                             <input type="radio" id="folder" name="type" value="folder" {...register('type', {required:"Type is required!!!"})}/>
-                            <label htmlFor="folder">Folder</label>
-                            <p>{errors?.type?.message}</p>
+                            <label htmlFor="folder" id="Folder">Folder</label>
                         </div>
+                        {errors?.type?.message && <Tooltip anchorId="Folder" content={errors?.type?.message} place="right" variant='error' isOpen /> }
                         <div className={styles.popup_input}>
                             <input type="text" id="name" name="name" placeholder='Name' {...register('name', {required:"File name is required!!!"})}/>
-                            <p>{errors?.name?.message}</p>
-                            <input type="text" id="creator" name="creatorName" placeholder='Creator' {...register('creatorName', {required:"Creator is required!!!"})}/>
-                            <p>{errors?.creator?.message}</p>
+                            {errors?.name?.message && <Tooltip anchorId="name" content={errors?.name?.message} place="right" variant='error' isOpen /> }
+                            <input type="text" id="creatorName" name="creatorName" placeholder='Creator' {...register('creatorName', {required:"Creator is required!!!"})}/>
+                            {errors?.creatorName?.message && <Tooltip anchorId="creatorName" content={errors?.creatorName?.message} place="right" variant='error' isOpen /> }
                             <input type="text" id="size" name="size" placeholder='Size' {...register('size', {required:"Size is required!!!"})}/>
-                            <p>{errors?.size?.message}</p>
-                            <input type="date" id="date" name="createdOn" placeholder='Date' {...register('createdOn', {required:"Date is required!!!"})}/>
-                            <p>{errors?.date?.message}</p>
+                            {errors?.size?.message && <Tooltip anchorId="size" content={errors?.size?.message} place="right" variant='error' isOpen /> }
+                            <input type="date" id="createdOn" name="createdOn" placeholder='Date' {...register('createdOn', {required:"Date is required!!!"})}/>
+                            {errors?.createdOn?.message && <Tooltip anchorId="createdOn" content={errors?.createdOn?.message} place="right" variant='error' isOpen /> }
                         </div>
                         <div className={styles.popup_submit_button}>
                             <button type='submit'>Create</button>
