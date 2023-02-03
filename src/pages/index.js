d4ae43e2b5843caafccfd6aa62e26ff091eadd1a
Priyajha7585 <priyajha7585@gmail.com>
2023-02-03 17:42:54 +0530

    Changing api link from localhost to vercel link

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,8 @@ export default function Home({folders}) {
 }
 
 export const getServerSideProps = async() => {
-  const res = await fetch('http://localhost:3000/api');
+  // const res = await fetch('http://localhost:3000/api');
+  const res = await fetch('https://file-system-ens8qe406-priyajha7585.vercel.app/api')
   const data = await res.json();
   return{
     props:{
